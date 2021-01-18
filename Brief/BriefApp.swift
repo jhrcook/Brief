1a745fee8e5fd7d1dc42cf0261d823e424468255
Josh Cook <joshuacook0023@gmail.com>
2021-01-18 06:46:09 -0500

    Initial Commit

---
+++ Brief/BriefApp.swift
@@ -0,0 +1,17 @@
+//
+//  BriefApp.swift
+//  Brief
+//
+//  Created by Joshua on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct BriefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
