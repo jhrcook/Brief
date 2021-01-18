354d0093c993a9354f47c4db9ac3f12fccb86884
Josh Cook <joshuacook0023@gmail.com>
2021-01-18 06:50:33 -0500

    pre-commit install and run all

--- BriefTests/BriefTests.swift
+++ BriefTests/BriefTests.swift
@@ -5,11 +5,10 @@
 //  Created by Joshua on 1/18/21.
 //
 
-import XCTest
 @testable import Brief
+import XCTest
 
 class BriefTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -25,9 +24,8 @@ class BriefTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
