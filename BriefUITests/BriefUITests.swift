1d3b37967fa720cc7acee8da42260516f68933b3
Josh Cook <joshuacook0023@gmail.com>
2021-03-08 06:50:58 -0500

    test: UI adding text and summarizing works

--- BriefUITests/BriefUITests.swift
+++ BriefUITests/BriefUITests.swift
@@ -8,26 +8,42 @@
 import XCTest
 
 class BriefUITests: XCTestCase {
+    var app: XCUIApplication!
+    var text: String!
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        app = XCUIApplication()
+        app.launch()
+
+        text = """
+           Here is a sentence lion bear oh my. Tiger bear leopard shoe. Walrus cup half goat. Closet shoe goat bear.
+        """
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    func testTapSummarizeButton() throws {
+        // Clear text, type in new text, hit the summarize button.
+        let swiftuiModifiedcontentBriefContentviewSwiftuiFlexframelayout1Appwindow1Window = XCUIApplication().windows["Brief"]
+        swiftuiModifiedcontentBriefContentviewSwiftuiFlexframelayout1Appwindow1Window.buttons["Clear"].click()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let textView = swiftuiModifiedcontentBriefContentviewSwiftuiFlexframelayout1Appwindow1Window.children(matching: .scrollView).element(boundBy: 0).children(matching: .textView).element
+        textView.click()
+        textView.typeText(text)
+        swiftuiModifiedcontentBriefContentviewSwiftuiFlexframelayout1Appwindow1Window.buttons["Summarize"].click()
+
+        // Test the output has been populated.
+        XCTAssertGreaterThanOrEqual(
+            swiftuiModifiedcontentBriefContentviewSwiftuiFlexframelayout1Appwindow1Window.staticTexts.count,
+            2
+        )
     }
 
     func testLaunchPerformance() throws {
